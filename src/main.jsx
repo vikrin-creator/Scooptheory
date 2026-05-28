774e6a2313eeb1ef60a54947d0d3e4630f47e2d9
sandanitin <sandanithin2@gmail.com>
2026-05-28 17:51:01 +0530

    fix: disable PWA service worker caching and add headers to prevent CDN caching of index.html

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,20 @@ import App from './App'
 import './index.css'
 import { reportWebVitals } from './utils/analytics'
 
+// Automatically unregister all service workers and clear caches to bypass PWA aggressive caching issues
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+      registration.unregister();
+    }
+  });
+}
+if ('caches' in window) {
+  caches.keys().then((keys) => {
+    keys.forEach((key) => caches.delete(key));
+  });
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
