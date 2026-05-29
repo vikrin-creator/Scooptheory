cc2a044d6eea857d257f64f871484d974943695a
sandanitin <sandanithin2@gmail.com>
2026-05-29 09:55:28 +0530

    refactor: switch router to HashRouter for server-independent robust routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import About from './pages/About';
 import Menu from './pages/Menu';
